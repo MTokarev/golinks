f862aa3ffc1c7bda22d7c81f3ddbd91142a11a59
Mikhail Tokarev <m.tokarev@hotmail.com>
2020-02-23 16:41:07 -0800

    change listener

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace golink
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://.*:80");
     }
 }
